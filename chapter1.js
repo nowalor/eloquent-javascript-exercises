016289a77920a9df7d13d6c1c9b532a952cb7df9
Nikulas Oskarsson <nikulasoskarsson@Nikulass-MacBook-Pro.local>
2024-09-08 14:58:17 +0000

    no message

---
+++ chapter1.js
@@ -0,0 +1,29 @@
+// Exercise 1
+function pyramid(){
+    let pyramidString = '#'
+
+    while(pyramidString.length <= 7) {
+        console.log(pyramidString)
+
+        pyramidString += '#'
+    }
+}
+
+//pyramid()
+
+// Exercise 2
+function fizzBuzz(){
+    for(let i = 0; i <= 100; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz')
+        } else if(i % 3 === 0) {
+            console.log('Fizz')
+        } else if(i % 5 === 0) {
+            console.log('Buzz')
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+fizzBuzz()
