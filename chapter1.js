9c1e4282dec0b959d23a1475d5d8a8e874c08904
Nikulas Oskarsson <nikulasoskarsson@Nikulass-MacBook-Pro.local>
2024-09-08 16:35:55 +0000

    no message

--- chapter1.js
+++ chapter1.js
@@ -9,11 +9,11 @@ function pyramid(){
     }
 }
 
-//pyramid()
+pyramid()
 
 // Exercise 2
 function fizzBuzz(){
-    for(let i = 0; i <= 100; i++) {
+    for(let i = ; i <= 100; i++) {
         if(i % 3 === 0 && i % 5 === 0) {
             console.log('FizzBuzz')
         } else if(i % 3 === 0) {
@@ -26,22 +26,22 @@ function fizzBuzz(){
     }
 }
 
-//fizzBuzz()
+fizzBuzz()
 
-function chessBoard() {
-    let size = 8
+// Exercise 3
+function chessBoard(size) {
     let chessBoardString = ''
     for(let i = 0; i < size; i++) {
         let previousChar
         if(i % 2 === 0) {
             previousChar = '#'
         } else {
-            previousChar = ''
+            previousChar = ' '
         }
 
         for (let x = 0; x < size; x++) {
             chessBoardString += previousChar
-            previousChar = previousChar === '#' ? '' : '#'
+            previousChar = previousChar === '#' ? ' ' : '#'
         }
 
         chessBoardString += '\n'
@@ -51,4 +51,22 @@ function chessBoard() {
     console.log(chessBoardString)
 }
 
-chessBoard()
+// ChatGPT solution
+function chessBoard2(size) {
+    let chessBoardString = '';
+
+    for (let i = 0; i < size; i++) {
+        for (let x = 0; x < size; x++) {
+            if ((i + x) % 2 === 0) {
+                chessBoardString += '#';
+            } else {
+                chessBoardString += ' ';
+            }
+        }
+        chessBoardString += '\n';
+    }
+
+    console.log(chessBoardString);
+}
+
+chessBoard(12)
